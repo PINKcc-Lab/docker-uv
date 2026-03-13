b806feb008030e9333a73e25faa8124afcf27067
ChadEstoupStreiff <chad.estoup-streiff@inserm.fr>
2026-03-13 12:38:31 +0100

    MPS not available in docker ...

--- src/main.py
+++ src/main.py
@@ -1,19 +1,28 @@
+import os
 import sys
+
 import torch
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
 
 def main():
     print(f"Python version: {sys.version}")
     print(f"PyTorch version: {torch.__version__}")
-    print(f"CUDA version: {torch.version.cuda}")
-    print(f"GPU: {'Available' if torch.cuda.is_available() else 'Not Available'}")
+    print(
+        f"GPU: {'Available CUDA' + torch.version.cuda if torch.cuda.is_available() else 'Not Available'}"
+    )
+    print(f"MPS (Apple Silicon) support: {'Available' if torch.backends.mps.is_available() else 'Not Available'}")
 
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
-    else:
-        device = "cpu"
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
     print(f"Using device: {device}")
-    
+
+
 if __name__ == "__main__":
     main()
